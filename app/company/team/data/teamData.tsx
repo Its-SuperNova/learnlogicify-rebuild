fac4f62658a31484916e72a10d1f1a1a15abb6c1
Its-SuperNova <its.ashwin.23@gmail.com>
2025-01-29 14:20:21 +0530

    Team section updated

--- app/company/team/data/teamData.tsx
+++ app/company/team/data/teamData.tsx
@@ -66,4 +66,17 @@ export const teamMembers: TeamMember[] = [
     bio: `Began his career as a Microsoft Integration Architect in London, UK in 2010. For the next 10 years, he specialized in a single technology—BizTalk. He worked as a BizTalk consultant for various top firms in the world like Accenture, Fidelity and Microsoft itself briefly, before he identified a gap in the BizTalk technology. His clients were facing the same issues and Microsoft had stopped focusing on the BizTalk server.
     Passionate about coding and building new products, Sachin in 2010 began building a software product that addresses all the gaps in the BizTalk server. A year later, he demonstrated a prototype of the product at an annual Microsoft conference in Seattle. People were blown away by the product’s potential. He then took all their feedback, polished it further and three months later released the product calling it BizTalk360. Sachin has grown the single-person product entity to a multiproduct, 290+ member organization with an ARR of $10 million, functioning out of London, UK, and Coimbatore, India.`, // Added the bio content
   },
+  {
+    name: "Hari",
+    nickname: "Shesha",
+    designation: "Technical Trainer",
+    image: "/images/team/arun x.png",
+    instagram: "https://www.instagram.com/yourprofile",
+    whatsapp: "https://wa.me/yourphonenumber",
+    linkedin: "https://www.linkedin.com/in/yourprofile",
+    mail: "mailto:your-email@example.com",
+    backgroundColor: "#E2D8FC",
+    bio: `Began his career as a Microsoft Integration Architect in London, UK in 2010. For the next 10 years, he specialized in a single technology—BizTalk. He worked as a BizTalk consultant for various top firms in the world like Accenture, Fidelity and Microsoft itself briefly, before he identified a gap in the BizTalk technology. His clients were facing the same issues and Microsoft had stopped focusing on the BizTalk server.
+    Passionate about coding and building new products, Sachin in 2010 began building a software product that addresses all the gaps in the BizTalk server. A year later, he demonstrated a prototype of the product at an annual Microsoft conference in Seattle. People were blown away by the product’s potential. He then took all their feedback, polished it further and three months later released the product calling it BizTalk360. Sachin has grown the single-person product entity to a multiproduct, 290+ member organization with an ARR of $10 million, functioning out of London, UK, and Coimbatore, India.`, // Added the bio content
+  },
 ];
