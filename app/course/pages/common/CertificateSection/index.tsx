c8a185e30dc9d31f063990398b6eacfcbec3fd80
Its-SuperNova <its.ashwin.23@gmail.com>
2025-02-15 10:19:31 +0530

    course certification section updated to tailwind

--- app/course/pages/common/CertificateSection/index.tsx
+++ app/course/pages/common/CertificateSection/index.tsx
@@ -1,39 +1,35 @@
 import React from "react";
-import styles from "./styles.module.css";
 import Link from "next/link";
 import Image from "next/image";
 
 const Certificate = () => {
   return (
-    <div className={styles.container}>
-      <div className={styles.certificate}>
+    <div className="w-full h-full border border-gray-300 rounded-2xl p-6 flex flex-row gap-10">
+      <div className="w-[850px] h-full ">
         <Image
-          className={styles.image}
+          className="w-full h-full rounded-2xl"
           src="/images/certificate/c-1.png"
           alt="Certificate of the course"
-          width={600} // Specify the width of the image
-          height={400} // Specify the height of the image
+          width={600}
+          height={400}
         />
       </div>
-      <div className={styles.content}>
-        <div className={styles.title}>
-          <h3>Certificate of the course</h3>
+      <div className="flex flex-col w-full gap-3 ">
+        <div className="text-2xl font-bold text-start">
+          Certificate of the Course
         </div>
-        <div className={styles.desc}>
-          <p>
-            Upon successfully completing the course, you will be awarded the
-            prestigious Certificate of Excellence from LearnLogicify
-            Technologies. This certificate is a testament to your dedication and
-            mastery of the subject, showcasing your commitment to continuous
-            learning and your ability to apply advanced skills. It will be a
-            valuable addition to your professional portfolio, helping you stand
-            out to potential employers and opening doors to new career
-            opportunities.
-          </p>
+        <div className="text-lg text-gray-600 leading-7 pr-10 lg:pr-0">
+          Upon successfully completing the course, you will be awarded the
+          prestigious Certificate of Excellence from LearnLogicify Technologies.
+          This certificate is a testament to your dedication and mastery of the
+          subject, showcasing your commitment to continuous learning and your
+          ability to apply advanced skills. It will be a valuable addition to
+          your professional portfolio, helping you stand out to potential
+          employers and opening doors to new career opportunities.
         </div>
         <div>
           <Link href="/images/certificate/c-1.png" passHref>
-            <div className={styles.btn}>
+            <div className="bg-purple-700 hover:bg-purple-800 text-white text-lg rounded-lg flex items-center justify-center cursor-pointer w-36 mt-2 z-1 h-10 md:text-base">
               View Certificate
             </div>
           </Link>
