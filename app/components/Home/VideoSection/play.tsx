b53f974f6c05f8059fac08f3b548479973308dc1
Its-SuperNova <its.ashwin.23@gmail.com>
2025-03-02 18:21:29 +0530

    play button bug fix

--- app/components/Home/VideoSection/play.tsx
+++ app/components/Home/VideoSection/play.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React from "react";
 import styled from "styled-components";
 import { FaPlay } from "react-icons/fa6";
@@ -6,7 +7,8 @@ const Button = () => {
   return (
     <StyledWrapper>
       <div>
-        <button className="c-button c-button--gooey">   
+        <button className="c-button c-button--gooey">
+   
           <FaPlay />
           <div className="c-button__blobs">
             <div />
